fcca7968bf0c048b00b2bedcd2041f7cac731bb5
lol760 <69092830+Alexandrsachuk@users.noreply.github.com>
2022-05-06 21:18:56 +0300

    Update create_database.py

--- trade_helper/create_database.py
+++ trade_helper/create_database.py
@@ -26,8 +26,8 @@ conn = engine.connect()
 Session = sessionmaker(bind=engine)
 s = Session()
 bot = Binance(
-    API_KEY='TH2gATkP9dGER8NIdws4gzJ7xR1hTWCjpWDm4wjGv8BvICmZnELhAjU1AifPsNZK',
-    API_SECRET='x9CBXLOR4THuwzp8mXwM5PtQiy5nXhnopLRCGZ4MnPAKgqsrwcBGeboXvsKbByXE'
+    API_KEY='...',
+    API_SECRET='...'
 )
 
 
